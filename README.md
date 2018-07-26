326190d5af4538dd62dc729c57478d0dad400448
chouyua <logsoi@126.com>
2018-07-26 23:41:15 +0800

    start flow

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # activiti_programatic
 #### activiti spring integration
 #### demo to show how to programatically create and deploy a flow
+
+flow of start a process.
+RuntimeServiceImpl
+ CommandExecutorImpl
+  LogInterceptor
+   SpringTransactionInterceptor
+    CommandContextInterceptor
+     CommandInvoker
+      StartProcessInstanceCmd
+       Find process definition by key from ACT_RE_PROCDEF
+       Find deployment by process definition id from ACT_RE_DEPLOYMENT
+       Find bpmn byte arrays by deployment id from ACT_GE_BYTEARRAY
--- src/main/java/z/learn/Activiti.java
+++ src/main/java/z/learn/Activiti.java
@@ -10,15 +10,20 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  */
 public class Activiti {
 
-    public static void main(String[] args) {
+    private static ApplicationContext context;
+    private static RuntimeService runtimeService;
+    private static String key = "demo";
 
-        ApplicationContext context = new ClassPathXmlApplicationContext("activiti.xml");
-        RuntimeService runtimeService = context.getBean("runtimeService", RuntimeService.class);
-        CreateAndDeployFlow createAndDeployFlow = context.getBean("createAndDeployFlow", CreateAndDeployFlow.class);
-
-        String key = "demo";
-        createAndDeployFlow.createAndDeploy(key);
+    public static void main(String[] args) throws InterruptedException {
+        context = new ClassPathXmlApplicationContext("activiti.xml");
+        runtimeService = context.getBean("runtimeService", RuntimeService.class);
         runtimeService.startProcessInstanceByKey(key);
-        System.out.println("aaa");
+
+        Thread.sleep(10 * 60 * 1000);
+    }
+
+    private static void prepare() {
+        CreateAndDeployFlow createAndDeployFlow = context.getBean("createAndDeployFlow", CreateAndDeployFlow.class);
+        createAndDeployFlow.createAndDeploy(key);
     }
 }
